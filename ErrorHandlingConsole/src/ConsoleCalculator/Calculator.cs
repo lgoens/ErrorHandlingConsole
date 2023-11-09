5a2cf01fd2e61e6ffef53304978b05bd88a03069
Leonardo Goens <lgoens@apexsystems.com>
2023-11-08 18:39:25 -0600

    Add calc process

---
+++ ErrorHandlingConsole/src/ConsoleCalculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleCalculator;
+
+public class Calculator
+{
+    public int Calculate(int number1, int number2, string operation)
+    {
+        if (operation.Equals("/"))
+        {
+            return Divide(number1, number2);
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException(nameof(operation), "The math operator is not supported");
+        }
+    }
+
+    private int Divide(int number, int divisor) => Math.DivRem(number, divisor).Quotient;
+}
--- ErrorHandlingConsole/src/ConsoleCalculator/Program.cs
+++ ErrorHandlingConsole/src/ConsoleCalculator/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleCalculator;
+using static System.Console;
+
+WriteLine("Enter first number");
+int number1 = int.Parse(ReadLine()!);
+
+WriteLine("Enter second number");
+int number2 = int.Parse(ReadLine()!);
+
+WriteLine("Enter operation");
+string operation = ReadLine()!.ToUpperInvariant();
+
+try
+{
+    var calculator = new Calculator();
+    int result = calculator.Calculate(number1, number2, operation);
+    DisplayResult(result);
+}
+catch (Exception ex)
+{
+    WriteLine($"Sorry, something went wrong. {ex}");
+}
+
+
+WriteLine("\nPress enter to exit.");
+ReadLine();
+
+static void DisplayResult(int result) => WriteLine($"Result is: {result}");
